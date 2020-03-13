827c9c0c8dd493ca60e45b738a0686324ccf452e
Fyodor Podshivadlov <fyodor.podshivadlov@outlook.com>
2020-03-13 18:23:16 +0300

    Added storybook with typescript and scss

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ['../src/stories/**/*.stories.ts'],
+  addons: [
+    '@storybook/preset-typescript',
+    '@storybook/preset-scss',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+  ],
+};
