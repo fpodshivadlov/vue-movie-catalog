ee6ed0401dc3d3d03241e742fc2a87879b3eeea3
Fyodor Podshivadlov <fyodor.podshivadlov@outlook.com>
2020-03-21 22:08:43 +0300

    Reduced bootstrap dependencies

--- src/router/index.ts
+++ src/router/index.ts
@@ -20,7 +20,7 @@ const routes: RouteConfig[] = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/MoviePage/MoviePage.vue'),
+    component: () => import(/* webpackChunkName: "details" */ '../views/MoviePage/MoviePage.vue'),
     meta: {
       title: 'Movie Details',
     },
