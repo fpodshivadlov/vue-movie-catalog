f8cae7e447edefcc8166079416859fe5518c888e
Fyodor Podshivadlov <fyodor.podshivadlov@outlook.com>
2020-03-22 18:15:17 +0300

    Updated vue configuration

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,8 +1,8 @@
-declare const VUE_APP_BACKEND_BASE_URL: string;
-declare const BACKEND_BASE_URL: string;
-
 interface Configuration {
   VUE_APP_BACKEND_BASE_URL: string;
 }
 
-export default process.env as unknown as Configuration;
+export default {
+  VUE_APP_BACKEND_BASE_URL: '/api/',
+  ...process.env,
+} as Configuration;
